fcc3c7edbb3f5cda3590c77cfe0dfe115c08bedb
eunji120 <eunji120@gmail.com>
2018-10-15 01:20:55 -0400

    created if-then statements for clicking

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Clicky-Game
 
 ## Description
-    This gaming application shows different images of which User selects one image by clicking. When they click, a score goes up by 1, and the screen shows the immages in random order. If User clicks the same image, the score drops back to 0. Once User's score is reset after an incorrect guess, the game restarts.
+    This gaming application shows different images (by Toronto's David Crighton) of which User selects one image by clicking. When they click, a score goes up by 1, and the screen shows the immages in random order. If User clicks the same image, the score drops back to 0. Once User's score is reset after an incorrect guess, the game restarts.
 
 ## Technologies Used
 - HTML
--- clickygame/src/App.js
+++ clickygame/src/App.js
@@ -2,7 +2,87 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+let correctGuesses = 0;
+let bestScore = 0;
+let clickMessage = "Click on an image to earn points. Do not click on the same image more than once!"
+
 class App extends Component {
+
+  //setting this.state.matches to the matches json array
+  state = {
+    //matches,
+    correctGuesses,
+    bestScore,
+    clickMessage
+  };
+
+  setClicked = id => {
+
+    //setting variables
+    const matches = this.state.matches;
+    const clickedMatch = matches.filter(match => match.id === id);
+
+    //if the matched img's clicked value is already true, do the game over actions
+    if (clickedMatch[0].clicked) {
+
+      console.log("Correct Guesses: " + correctGuesses);
+      console.log("Best Score: " + bestScore);
+
+      correctGuesses = 0;
+      clickMessage = "Start over"
+
+      for (let i = 0; i < matches.length; i++) {
+        matches[i].clicked = false;
+      }
+
+      this.setState({clickMessage});
+      this.setState({correctGuesses});
+      this.setState({matches});
+
+      // if clicked = false, and the user hasn't completed
+    } else if (correctGuesses < 11) {
+
+      clickedMatch[0].clicked = true;
+      correctGuesses++;
+      clickMessage = "Keep going!"
+
+      if (correctGuesses > bestScore) {
+        bestScore = correctGuesses;
+        this.setState({bestScore});
+      }
+
+      //shuffling the array to be shown in a random order
+      matches.sort(function(a, b) {
+        return 0.5 - Math.random()
+      });
+
+      this.setState({matches});
+      this.setState({correctGuesses});
+      this.setState({clickMessage});
+
+      //if the player selected different images 12 times
+    } else {
+
+      clickedMatch[0].clicked = true;
+      correctGuesses= 0;
+      clickMessage = "Great work!"
+      bestScore = 12;
+      this.setState({bestScore});
+
+      for (let i = 0; i < matches.length; i++) {
+        matches[i].clicked = false;
+      }
+
+      matches.sort(function(a, b) {
+        return 0.5 - Math.random()
+      });
+
+      this.setState({matches});
+      this.setState({correctGuesses});
+      this.setState({clickMessage});
+    }
+  };
+
   render() {
     return (
       <div className="App">
